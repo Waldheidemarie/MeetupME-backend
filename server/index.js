779a596a4cbc7e9df79bc83a65c58350b5b58c0f
scott <you@example.comitsmescottb123@gmail.com>
2018-06-08 11:35:41 -0400

    backend initial

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dbConfig from './config/db'
+import middlewareConfig from './config/middleware'
+import { MeetupRoutes } from './modules'
+
+const app = express()
+
+/**
+  DATABASE
+**/
+dbConfig()
+
+/**
+  MIDDLEWARE
+**/
+middlewareConfig(app)
+
+app.use('/api', [MeetupRoutes])
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`Listening on port ${PORT}!`);
+  }
+})
